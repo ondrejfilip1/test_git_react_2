0511828451d4fd70e969d5474d97d9781f97dae4
Jan Rylich <rylich.honza@email.cz>
2025-02-24 14:30:58 +0100

    eshop

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,9 +3,12 @@ import Home from "./Home";
 import Info from "./Info";
 import About from "./About";
 import Contact from "./cont";
+<<<<<<< HEAD
 import Photos from "./photos";
 import EshopHodinky from "./EschopHodinky";
 import Casket from "./Casket";
+=======
+>>>>>>> f3030bf (eshop)
 
 export default function AppRoutes() {
   return (
@@ -16,9 +19,12 @@ export default function AppRoutes() {
           <Route path="/info" element={<Info />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
+<<<<<<< HEAD
           <Route path="/photos" element={<Photos />} />
           <Route path="/ehodinky" element={<EshopHodinky />} />
           <Route path="/casket" element={<Casket />} />
+=======
+>>>>>>> f3030bf (eshop)
         </Routes>
       </BrowserRouter>
     </>
