ad9531129fc3788baabac8918ccbfeb9ff4e1ad2
Jan Rylich <rylich.honza@email.cz>
2025-02-24 14:17:50 +0100

    info page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Info from "./Info";
 
 export default function AppRoutes() {
   return (
@@ -7,6 +8,7 @@ export default function AppRoutes() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />} />
         </Routes>
       </BrowserRouter>
     </>
