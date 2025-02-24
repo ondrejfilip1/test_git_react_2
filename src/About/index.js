5d7d5cc21610d241329fc7110ed5fa8007a231de
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-02-24 15:05:39 +0100

    Revert "Merge branch 'main' into feature3"
    This reverts commit 4173cfb1ab2a066664cc639d1e691310327b16cd, reversing
    changes made to 0e57395e300f8ebc9d6c4dbcc099e05860de100b.

--- src/About/index.js
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-      <>
-        <h1>ABOUT stránka</h1>
-        <p>ABOUT</p>
-      </>
-    );
-  }
-  
--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,14 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Info from "./Info";
-import About from "./About";
 import Contact from "./cont";
-<<<<<<< HEAD
-import Photos from "./photos";
-import EshopHodinky from "./EschopHodinky";
-import Casket from "./Casket";
-=======
->>>>>>> f3030bf (eshop)
+import About from "./About";
 
 export default function AppRoutes() {
   return (
@@ -19,12 +13,6 @@ export default function AppRoutes() {
           <Route path="/info" element={<Info />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
-<<<<<<< HEAD
-          <Route path="/photos" element={<Photos />} />
-          <Route path="/ehodinky" element={<EshopHodinky />} />
-          <Route path="/casket" element={<Casket />} />
-=======
->>>>>>> f3030bf (eshop)
         </Routes>
       </BrowserRouter>
     </>
